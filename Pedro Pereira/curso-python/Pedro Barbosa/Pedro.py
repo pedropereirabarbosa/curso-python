36fe41c6f783a48435bc5d57b348847fd3d6b1d2
Pedro Barbosa <50033688+pedropereirabarbosa@users.noreply.github.com>
2026-02-11 22:01:43 -0300

    Add files via upload

---
+++ Pedro Pereira/curso-python/Pedro Barbosa/Pedro.py
@@ -0,0 +1,11 @@
+print("Meu nome é Pedro e estou aprendendo Python!")
+
+nome_completo = "Maria Jose da Silva"
+NomeCompleto = "Jose de Alexandre"
+nomeCompleto = "Joao Gomes"
+nomecompletoproprio = "test"
+
+print(nome_completo)
+print(nomeCompleto)
+print(NomeCompleto)
+print(nomecompletoproprio)
