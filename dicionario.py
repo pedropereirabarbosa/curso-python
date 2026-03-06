73f0b8b58f0ea059b2c26b543b45533bb4dd43fa
Pedro Barbosa <50033688+pedropereirabarbosa@users.noreply.github.com>
2026-03-05 21:51:28 -0300

    Add files via upload
    Adicionados os arquivos da aula 10.

---
+++ dicionario.py
@@ -0,0 +1,22 @@
+pessoa = {
+    "profissao": "professor",
+    "nacionalidade": "brasileira",
+    "idade": 45,
+    "estado_civil": "solteiro"
+}
+
+print(pessoa["estado_civil"])
+print(pessoa["idade"])
+print(pessoa["profissao"])
+print(pessoa["nacionalidade"])
+
+pessoa["nome"] = "João"
+print(pessoa["nome"])
+
+pessoa["cor_dos_olhos"] = "Castanhos"
+pessoa["altura"] = "178cm"
+pessoa["peso"] = "74kg"
+
+print(pessoa["cor_dos_olhos"])
+print(pessoa["altura"])
+print(pessoa["peso"])
---
+++ reqs.py
@@ -0,0 +1,9 @@
+import requests as req
+
+# cep = input("Digite seu cep: ")
+# response = req.get(f"https://viacep.com.br/ws/{cep}/json")
+
+# data = response.json()
+
+# print(data)
+# print(f'Você mora na {data['logradouro']} que fica no bairro {data['bairro']}.')
